96b9ffd92fa77024d91e012ab3175c74c282fab4
johnbuns <johnbuns@yahoo.com>
2016-04-13 22:03:55 -0700

    PerkTV - More detailed logging.

--- lib/PerkTV.sh
+++ lib/PerkTV.sh
@@ -1,6 +1,7 @@
 perkTVPackage="com.juteralabs.perktv"
 perkTVPressActivity="com.juteralabs.perktv.activities.WatchEarnOverlay"
 perkTVLogName="perkTV"
+perkTVHealthCheckLogName="perkTVHealthCheck"
 
 launchPerkTV() {
 	launchPerkTV2
@@ -21,16 +22,19 @@ launchPerkTV2() {
 	dumpScreen
 	
 	if [ $(isValueOnScreen "ib_fan_close") == "true" ]; then
+		logStuff $perkTVLogName "ib_fan_close"
 		normalTouch 304 56
 		sleep 1
 	fi
 	
 	# Watch & Earn title
 	waitUntilTextFound "Watch &amp; Earn"
+	logStuff $perkTVLogName "Watch &amp; Earn"
 	boundedTouch 55 160 108 203
 	
 	# Anastasia Date
 	waitUntilTextFound "Anastasia Date"
+	logStuff $perkTVLogName "Anastasia Date"
 	boundedTouch 34 101 299 216
 }
 
@@ -39,19 +43,19 @@ perkTVHealthCheck() {
 	standardHealthCheck
 	
 	if [ $(isValueOnScreen "ib_fan_close") == "true" ]; then
-		logStuff $perkTVLogName "Back on the start screen with the static ad."
+		logStuff $perkTVHealthCheckLogName "Back on the start screen with the static ad."
 		restartTheWholeThing
 		return
 	fi
 	
 	if [ $(isValueOnScreen "Watch &amp; Earn") == "true" ]; then
-		logStuff $perkTVLogName "Back on the start screen without the static ad."
+		logStuff $perkTVHealthCheckLogName "Back on the start screen without the static ad."
 		restartTheWholeThing
 		return
 	fi
 	
 	if [ $(isValueOnScreen "text=\"X\"") == "true" ]; then
-		logStuff $perkTVLogName "Found static ad to close."
+		logStuff $perkTVHealthCheckLogName "Found static ad to close."
 		normalTouch 455 25
 		return
 	fi
